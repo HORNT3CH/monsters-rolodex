486d67207c4b2857e530d48e78a234e13ae8ad18
HORNT3CH <cmfrtblynmb@neo.rr.com>
2019-08-24 09:22:10 -0400

    Updates

---
+++ precache-manifest.6622dcb79d974c7c7f7881a919f6c294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c0d839bd0338050216ce9a3230f2fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e230f6b983f08d825ec9",
+    "url": "/monsters-rolodex/static/css/main.7d352a5a.chunk.css"
+  },
+  {
+    "revision": "c7b4e4d826c083e3a4d0",
+    "url": "/monsters-rolodex/static/js/2.5e22ffbf.chunk.js"
+  },
+  {
+    "revision": "e230f6b983f08d825ec9",
+    "url": "/monsters-rolodex/static/js/main.a4a5013e.chunk.js"
+  },
+  {
+    "revision": "64a6a9ea7df02e318e9c",
+    "url": "/monsters-rolodex/static/js/runtime~main.01ade24d.js"
+  }
+]);
