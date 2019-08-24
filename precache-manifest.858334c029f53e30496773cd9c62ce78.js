a7700c4407d4233b022f221ed312927f1cdaaabd
HORNT3CH <cmfrtblynmb@neo.rr.com>
2019-08-24 08:38:41 -0400

    Updates

---
+++ precache-manifest.858334c029f53e30496773cd9c62ce78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0773d5a405b497dd54493a67d03ad7",
+    "url": "/HORNT3CH/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "1346e8e8c85ca6713a1a",
+    "url": "/HORNT3CH/monsters-rolodex.git/static/css/main.7d352a5a.chunk.css"
+  },
+  {
+    "revision": "c7b4e4d826c083e3a4d0",
+    "url": "/HORNT3CH/monsters-rolodex.git/static/js/2.5e22ffbf.chunk.js"
+  },
+  {
+    "revision": "1346e8e8c85ca6713a1a",
+    "url": "/HORNT3CH/monsters-rolodex.git/static/js/main.8e3606ac.chunk.js"
+  },
+  {
+    "revision": "c874bf8f9c7ac12f08dc",
+    "url": "/HORNT3CH/monsters-rolodex.git/static/js/runtime~main.29d9d01f.js"
+  }
+]);
